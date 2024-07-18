e217fb80246731cce09c9aafbedf7266cdd4945b
almaz amanzholuly <almaz.amanzholuly@nu.edu.kz>
2024-07-18 17:58:00 +0500

    Added: user login and create router

--- app/router/main/funding_data.py
+++ app/router/main/funding_data.py
@@ -1,18 +1,14 @@
-import pandas as pd
-import numpy as np
-import requests
-from fastapi import HTTPException, APIRouter
-
-
-router = APIRouter()
-
-
-@router.get("/get_funding_data")
-def get_funding_data():
-    url = "https://fapi.binance.com/fapi/v1/fundingRate"
-    response = requests.get(url)
-    if response.status_code == 200:
-        funding_data = pd.DataFrame(response.json())
-        funding_data['fundingRate'] = np.round(funding_data.fundingRate.astype(float) * 100, 5)
-        return funding_data.to_dict(orient="records")
-
+# import requests
+# from fastapi import HTTPException, APIRouter
+#
+#
+# router = APIRouter()
+#
+#
+# @router.get("/get_funding_data")
+# def get_funding_data():
+#     url = "https://fapi.binance.com/fapi/v1/fundingRate"
+#     response = requests.get(url)
+#     # TODO: make this function for providing the information
+#     if response.status_code == 200:
+#         pass
