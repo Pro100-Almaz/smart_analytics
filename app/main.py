c8eefa367bd4d98e826ae6f93dc608965fce7c29
almaz amanzholuly <almaz.amanzholuly@nu.edu.kz>
2024-07-18 10:46:14 +0500

    Update: main file

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
+import uvicorn
 from fastapi import FastAPI
+from app.database import database
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.webhook import router as webhook_router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(webhook_router, prefix="/webhook")
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
