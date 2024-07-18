fa9dc22e2c37024af17bdcd505b0418e1c4ba141
almaz amanzholuly <almaz.amanzholuly@nu.edu.kz>
2024-07-18 12:35:19 +0500

    fix

--- app/main.py
+++ app/main.py
@@ -18,5 +18,14 @@ app.add_middleware(
 app.include_router(webhook_router, prefix="/webhook")
 
 
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
