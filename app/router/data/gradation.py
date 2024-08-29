e9667578953b68267cf2a8b49e0c566516f86d60
almaz amanzholuly <almaz.amanzholuly@nu.edu.kz>
2024-08-30 00:16:53 +0500

    fix

--- app/router/data/gradation.py
+++ app/router/data/gradation.py
@@ -98,7 +98,7 @@ async def get_gradation(interval: int = Query(30), growth_type: str = Query("Vol
             """, user_id, current_date, current_time, file_name, "volume" if growth_type == "Volume" else "price"
         )
 
-        return {"status": "success", "file_name": f"volume_growth_{interval}.csv", "file_id": file_id}
+        return {"status": "success", "file_name": f"volume_growth_{interval}.csv", "file_id": file_id[0][0]}
 
     return {"status": "service_error"}
 
