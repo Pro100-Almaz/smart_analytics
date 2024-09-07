b4e961bf7db070aedb3c6676b29a992f61e1f496
almaz amanzholuly <almaz.amanzholuly@nu.edu.kz>
2024-09-07 22:03:47 +0500

    fix

--- app/router/notify/__init__.py
+++ app/router/notify/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from .impulse import router as impulse_router
+from .ticker_tracking import router as ticker_tracking_router
 
 
 router = APIRouter()
 
 router.include_router(impulse_router)
+router.include_router(ticker_tracking_router)
