2aba92cb4fdee09eea0d6cfe414284b43f0c3b87
Almaz Amanzholuly <almaz.amanzholuly@nu.edu.kz>
2024-10-07 18:25:23 +0500

    fix

--- app/router/notify/ticker_tracking.py
+++ app/router/notify/ticker_tracking.py
@@ -68,7 +68,7 @@ async def get_ticker_tracking(token_data: Dict = Depends(JWTBearer())):
     return {"status": status.HTTP_200_OK, "records": records, "conditions": conditions}
 
 
-@router.get("/get_ticker_tracking_history", tags=["notify"])
+@router.get("/get_ticker_tracking_history", tags=["notify"], dependencies=[Depends(JWTBearer())])
 async def get_ticker_tracking_history(tt_id: int = Query()):
     ticker_tracking_history = await database.fetch(
         f"""
