e319ec7c3056c25e772913eac0eeb36b6c52ef06
1Daniyar1 <dsasenov01@gmail.com>
2024-10-10 16:09:59 +0200

    Added background_tasks to funding_data

--- app/router/data/funding_data.py
+++ app/router/data/funding_data.py
@@ -14,12 +14,54 @@ from app.auth_bearer import JWTBearer
 load_dotenv()
 router = APIRouter()
 
+async def file_generation(funding_data, interval, csv_file_path):
+    for record in funding_data:
+        stock_id = await database.fetchrow(
+            """
+            SELECT stock_id
+            FROM data_history.funding
+            WHERE symbol = $1;
+            """, record["symbol"]
+        )
+        
+        if not stock_id:
+            continue
+
+        stock_id = stock_id.get("stock_id")
+
+        stock_data = await database.fetch(
+            """
+            WITH FilteredData AS (
+                SELECT
+                    * ,
+                    ROW_NUMBER() OVER (ORDER BY funding_time) AS rn
+                FROM
+                    data_history.funding_data
+                WHERE
+                    stock_id = $1
+            )
+            SELECT
+                *
+            FROM
+                FilteredData
+            WHERE
+                rn % $2 = 0  
+            ORDER BY
+                funding_time;
+            """, stock_id, interval
+        )
+
+        for data in stock_data:
+            with open(csv_file_path, mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([record['symbol'], data['funding_time'], data['funding_rate'], data['mark_price']])
+
 
 @router.get("/funding_data", tags=["data"])
 async def get_funding_data(background_tasks: BackgroundTasks, interval: int = Query(7)):
     funding_response = requests.get("https://fapi.binance.com/fapi/v1/premiumIndex")
     if funding_response.status_code == 200:
-        user_id = 1 # token_data.get("user_id")
+        user_id = 1  # token_data.get("user_id")
         csv_file_path = f"dataframes/funding_data_{user_id}.csv"
 
         with open(csv_file_path, mode='w', newline='') as file:
@@ -60,64 +102,6 @@ async def get_funding_data(background_tasks: BackgroundTasks, interval: int = Qu
                 negative_funding_rate_quantity += 1
                 return_value['negative'][0] += 1
 
-            stock_id = await database.fetchrow(
-                """
-                SELECT stock_id
-                FROM data_history.funding
-                WHERE symbol = $1;
-                """, record["symbol"]
-            )
-
-            if not stock_id:
-                return {"status": status.HTTP_200_OK,
-                        "positive_quantity": positive_funding_rate_quantity,
-                        "negative_quantity": negative_funding_rate_quantity,
-                        "neutral_quantity": neutral_funding_rate_quantity,
-                        }
-
-            stock_id = stock_id.get("stock_id")
-
-            stock_data = await database.fetch(
-                """
-                WITH FilteredData AS (
-                    SELECT
-                        *,
-                        ROW_NUMBER() OVER (ORDER BY funding_time) AS rn
-                    FROM
-                        data_history.funding_data
-                    WHERE
-                        stock_id = $1
-                )
-                SELECT
-                    *
-                FROM
-                    FilteredData
-                WHERE
-                    rn % $2 = 0  
-                ORDER BY
-                    funding_time;
-                """, stock_id, time_gap
-            )
-
-            for data in stock_data:
-                if data.get("rn") not in return_value['time_interval']:
-                    return_value['time_interval'].append(data.get("rn"))
-                    return_value['positive'].append(0)
-                    return_value['negative'].append(0)
-                    return_value['neutral'].append(0)   
-
-                if data.get('funding_rate') > 0.01:
-                    return_value['positive'][-1] += 1
-                if data.get('funding_rate') != 0.005 or data.get('funding_rate') < 0.01:
-                    return_value['negative'][-1] += 1
-                if data.get('funding_rate') == 0.01 or data.get('funding_rate') == 0.05:
-                    return_value['neutral'][-1] += 1
-
-            with open(csv_file_path, mode='a', newline='') as file:
-                writer = csv.writer(file)
-
-                writer.writerow([record['symbol'], record['time'], float(record['lastFundingRate']) * 100, record['markPrice']])
-
         await database.execute(
             """
             INSERT INTO data_history.funding_data_history (user_id, created, positive_count, negative_count, neutral_count)
@@ -125,12 +109,16 @@ async def get_funding_data(background_tasks: BackgroundTasks, interval: int = Qu
             """, user_id, datetime.now(), positive_funding_rate_quantity, negative_funding_rate_quantity, neutral_funding_rate_quantity
         )
 
-        return {"status": status.HTTP_200_OK,
-                "positive_quantity": positive_funding_rate_quantity,
-                "negative_quantity": negative_funding_rate_quantity,
-                "neutral_quantity": neutral_funding_rate_quantity,
-                "graph_data": return_value
-                }
+
+        background_tasks.add_task(file_generation, funding_data, interval, csv_file_path)
+
+        return {
+            "status": status.HTTP_200_OK,
+            "positive_quantity": positive_funding_rate_quantity,
+            "negative_quantity": negative_funding_rate_quantity,
+            "neutral_quantity": neutral_funding_rate_quantity,
+            "graph_data": return_value
+        }
 
     return {"status": "service_error"}
 
